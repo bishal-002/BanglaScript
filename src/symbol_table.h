f0461b9c118cbd050df49703ea6650d4d2adae15
bishal-002 <bdhar262@gmail.com>
2026-08-10 21:14:04 +0600

    symbol table

---
+++ src/symbol_table.h
@@ -0,0 +1,19 @@
+#ifndef SYMBOL_TABLE_H
+#define SYMBOL_TABLE_H
+
+#include <string>
+#include <unordered_map>
+
+class SymbolTable
+{
+private:
+    std::unordered_map<std::string, std::string> symbols;
+
+public:
+    bool declare(const std::string& name,
+                 const std::string& type);
+
+    bool exists(const std::string& name) const;
+};
+
+#endif
